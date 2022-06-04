017b23cba27afa4b8e97c8f4edaa676a3300d4eb
Markus Lampe <idnkhbtfu@gmail.com>
2022-06-04 18:23:33 +0200

    Fade In of Pages

--- app/visualrb/ui/pages/about.js
+++ app/visualrb/ui/pages/about.js
@@ -1,6 +1,6 @@
 app.visualrb.ui.pages.about = ((page) => {
   const template = `
-    <div id="page-about" class="page">
+    <div id="page-about" class="page loading">
       <div class="page-header">
         <div class="page-icon icon-info"></div>
         <div class="page-title">about this tool</div>
@@ -15,6 +15,11 @@ app.visualrb.ui.pages.about = ((page) => {
     }
 
     setTimeout(() => container.innerHTML = template, 400)
+    setTimeout(() => {
+      for (page of container.getElementsByClassName('page')) {
+        page.classList.remove('loading')
+      }
+    }, 420);
   }
 
   return page
